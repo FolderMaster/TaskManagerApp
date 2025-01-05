7b51fb64248ee6faaa8643a11a8d86f4c1bbc4d4
FolderMaster <99458522+FolderMaster@users.noreply.github.com>
2025-01-05 15:52:09 +0700

    Fix code and diagrams.

--- src/MachineLearning/Converters/BaseLearningConverter.cs
+++ src/MachineLearning/Converters/BaseLearningConverter.cs
@@ -1,6 +1,4 @@
-﻿using Accord.Math;
-
-using MachineLearning.Interfaces;
+﻿using MachineLearning.Interfaces;
 using MachineLearning.Interfaces.Generals;
 
 namespace MachineLearning.Converters
@@ -24,11 +22,6 @@ namespace MachineLearning.Converters
         protected readonly IPrimaryPointDataProcessor _primaryPointDataProcessor;
 
         /// <summary>
-        /// Обработчики точечных данных.
-        /// </summary>
-        protected readonly IEnumerable<IPointDataProcessor> _pointDataProcessors;
-
-        /// <summary>
         /// Индексы удалённых столбцов.
         /// </summary>
         protected IEnumerable<int>? _removedColumnsIndices;
@@ -42,13 +35,13 @@ namespace MachineLearning.Converters
         /// Создаёт экземпляр класса <see cref="BaseLearningConverter{R, D, DT, DR}"/>.
         /// </summary>
         /// <param name="primaryPointDataProcessor">Первичный обработчик точечных данных.</param>
-        /// <param name="pointDataProcessors">Обработчики точечных данных.</param>
-        protected BaseLearningConverter(IPrimaryPointDataProcessor primaryPointDataProcessor,
-            IEnumerable<IPointDataProcessor> pointDataProcessors)
+        protected BaseLearningConverter(IPrimaryPointDataProcessor primaryPointDataProcessor)
         {
             _primaryPointDataProcessor = primaryPointDataProcessor;
-            _pointDataProcessors = pointDataProcessors;
         }
+
+        /// <inheritdoc />
+        public abstract DR ConvertPredicted(R predicted);
 
         /// <inheritdoc />
         public IEnumerable<double> ConvertData(DT data)
@@ -64,110 +57,11 @@ namespace MachineLearning.Converters
             return featuresScalersResult;
         }
 
-        /// <inheritdoc />
-        public abstract DR ConvertPredicted(R predicted);
-
-        /// <summary>
-        /// Обрабатывает признаков данных.
-        /// </summary>
-        /// <param name="data">Данные.</param>
-        /// <returns>Возвращает данные о преобразовании признаков данных.</returns>
-        protected DataProcessorResult<IEnumerable<double>> ProcessFeaturesData
-            (IEnumerable<D> data)
-        {
-            var featuresArray = ProcessFeatures(data).To2dArray();
-            var removedColumnsIndicesGroups = new List<IEnumerable<int>>();
-            var removedRowsIndicesGroups = new List<IEnumerable<int>>();
-
-            var primaryProcessorResult = _primaryPointDataProcessor.Process(featuresArray);
-            removedColumnsIndicesGroups.Add(primaryProcessorResult.RemovedColumnsIndices);
-            removedRowsIndicesGroups.Add(primaryProcessorResult.RemovedRowsIndices);
-            var newFeaturesArray = primaryProcessorResult.Result;
-
-            foreach (var pointDataProcessor in _pointDataProcessors)
-            {
-                var processorResult = pointDataProcessor.Process(newFeaturesArray);
-                removedColumnsIndicesGroups.Add(processorResult.RemovedColumnsIndices);
-                removedRowsIndicesGroups.Add(processorResult.RemovedRowsIndices);
-                newFeaturesArray = processorResult.Result;
-            }
-
-            var removedColumnsIndices = NormalizeRemovedIndices(removedColumnsIndicesGroups);
-            var removedRowIndices = NormalizeRemovedIndices(removedRowsIndicesGroups);
-            return new DataProcessorResult<IEnumerable<double>>
-                (newFeaturesArray, removedColumnsIndices, removedRowIndices);
-        }
-
-        /// <summary>
-        /// Создаёт коллекцию масштабирования данных.
-        /// </summary>
-        /// <param name="featuresArray">Массив признаков.</param>
-        protected void CreateScalers(double[][] featuresArray)
-        {
-            var columnCount = featuresArray.First().Length;
-            var scalers = new List<IScaler>();
-            for (var i = 0; i < columnCount; ++i)
-            {
-                var scaler = CreateScaler(i, _removedColumnsIndices);
-                var column = featuresArray.GetColumn(i);
-                var newColumn = scaler.FitTransform(column).ToArray();
-                featuresArray.SetColumn(i, newColumn);
-                scalers.Add(scaler);
-            }
-            _scalers = scalers;
-        }
-
-        /// <summary>
-        /// Обрабатывает признаков данных.
-        /// </summary>
-        /// <param name="data">Данные.</param>
-        /// <returns>Возвращает обработанные признаков данных.</returns>
-        protected abstract IEnumerable<IEnumerable<double?>> ProcessFeatures(IEnumerable<D> data);
-
         /// <summary>
         /// Извлекает признаки у элемента данных.
         /// </summary>
         /// <param name="dataItem">Элемент данных.</param>
         /// <returns>Возвращет, извлеченное у элемента данных, целевое значение.</returns>
         protected abstract IEnumerable<double?> ExtractFeatures(DT dataItem);
-
-        /// <summary>
-        /// Создаёт масштабирование данных.
-        /// </summary>
-        /// <param name="index">Индекс.</param>
-        /// <param name="removedColumnsIndices">Индексы удалённых столбцов.</param>
-        /// <returns>Возвращает масштабирование данных.</returns>
-        protected abstract IScaler CreateScaler(int index, IEnumerable<int> removedColumnsIndices);
-
-        /// <summary>
-        /// Нормализует индексы удалённых индексов.
-        /// </summary>
-        /// <param name="removedIndicesGroups">Группы индексов удалённых индексов.</param>
-        /// <returns>Возвращает нормализованные удалённые индексы.</returns>
-        private IEnumerable<int> NormalizeRemovedIndices
-            (IEnumerable<IEnumerable<int>> removedIndicesGroups)
-        {
-            var result = new List<int>();
-            foreach (var removedIndices in removedIndicesGroups)
-            {
-                var newNormalizedIndices = new List<int>();
-                foreach (var index in removedIndices.Order())
-                {
-                    var normalizedIndex = index;
-                    foreach (var removedIndex in result)
-                    {
-                        if (normalizedIndex > removedIndex)
-                        {
-                            break;
-                        }
-                        ++normalizedIndex;
-                    }
-                    newNormalizedIndices.Add(normalizedIndex);
-                }
-                result.AddRange(newNormalizedIndices);
-                result = result.Order().ToList();
-            }
-            return result;
-        }
     }
 }
--- src/MachineLearning/Converters/BaseSupervisedLearningConverter.cs
+++ src/MachineLearning/Converters/BaseSupervisedLearningConverter.cs
@@ -22,14 +22,21 @@ namespace MachineLearning.Converters
         ISupervisedLearningConverter<IEnumerable<double>, R, D, DT, DR>
     {
         /// <summary>
+        /// Обработчики точечных данных.
+        /// </summary>
+        protected readonly IEnumerable<IPointDataProcessor> _pointDataProcessors;
+
+        /// <summary>
         /// Создаёт экземпляр класса <see cref="BaseSupervisedLearningConverter{R, D, DT, DR}"/>.
         /// </summary>
         /// <param name="primaryPointDataProcessor">Первичный обработчик точечных данных.</param>
         /// <param name="pointDataProcessors">Обработчики точечных данных.</param>
         protected BaseSupervisedLearningConverter
             (IPrimaryPointDataProcessor primaryPointDataProcessor,
-            IEnumerable<IPointDataProcessor> pointDataProcessors) :
-            base(primaryPointDataProcessor, pointDataProcessors) { }
+            IEnumerable<IPointDataProcessor> pointDataProcessors) : base(primaryPointDataProcessor)
+        {
+            _pointDataProcessors = pointDataProcessors;
+        }
 
         /// <inheritdoc />
         public LearningModelData<IEnumerable<double>, R> FitConvertData
@@ -45,6 +52,109 @@ namespace MachineLearning.Converters
             var targets = ProcessTargets(dataArray, featuresResult.RemovedRowsIndices);
 
             return new LearningModelData<IEnumerable<double>, R>(featuresArray, targets);
+        }
+
+        /// <summary>
+        /// Обрабатывает целевые значения элемента.
+        /// </summary>
+        /// <param name="item">Элемента.</param>
+        /// <returns>Возвращет обработанное целевое значение элемента.</returns>
+        protected abstract R ProcessTarget(D item);
+
+        /// <summary>
+        /// Обрабатывает признаков данных.
+        /// </summary>
+        /// <param name="data">Данные.</param>
+        /// <returns>Возвращает обработанные признаков данных.</returns>
+        protected abstract IEnumerable<IEnumerable<double?>> ProcessFeatures(IEnumerable<D> data);
+
+        /// <summary>
+        /// Создаёт масштабирование данных.
+        /// </summary>
+        /// <param name="index">Индекс.</param>
+        /// <param name="removedColumnsIndices">Индексы удалённых столбцов.</param>
+        /// <returns>Возвращает масштабирование данных.</returns>
+        protected abstract IScaler CreateScaler(int index, IEnumerable<int> removedColumnsIndices);
+
+        /// <summary>
+        /// Обрабатывает признаков данных.
+        /// </summary>
+        /// <param name="data">Данные.</param>
+        /// <returns>Возвращает данные о преобразовании признаков данных.</returns>
+        protected DataProcessorResult<IEnumerable<double>> ProcessFeaturesData
+            (IEnumerable<D> data)
+        {
+            var featuresArray = ProcessFeatures(data).To2dArray();
+            var removedColumnsIndicesGroups = new List<IEnumerable<int>>();
+            var removedRowsIndicesGroups = new List<IEnumerable<int>>();
+
+            var primaryProcessorResult = _primaryPointDataProcessor.Process(featuresArray);
+            removedColumnsIndicesGroups.Add(primaryProcessorResult.RemovedColumnsIndices);
+            removedRowsIndicesGroups.Add(primaryProcessorResult.RemovedRowsIndices);
+            var newFeaturesArray = primaryProcessorResult.Result;
+
+            foreach (var pointDataProcessor in _pointDataProcessors)
+            {
+                var processorResult = pointDataProcessor.Process(newFeaturesArray);
+                removedColumnsIndicesGroups.Add(processorResult.RemovedColumnsIndices);
+                removedRowsIndicesGroups.Add(processorResult.RemovedRowsIndices);
+                newFeaturesArray = processorResult.Result;
+            }
+
+            var removedColumnsIndices = NormalizeRemovedIndices(removedColumnsIndicesGroups);
+            var removedRowIndices = NormalizeRemovedIndices(removedRowsIndicesGroups);
+            return new DataProcessorResult<IEnumerable<double>>
+                (newFeaturesArray, removedColumnsIndices, removedRowIndices);
+        }
+
+        /// <summary>
+        /// Создаёт коллекцию масштабирования данных.
+        /// </summary>
+        /// <param name="featuresArray">Массив признаков.</param>
+        protected void CreateScalers(double[][] featuresArray)
+        {
+            var columnCount = featuresArray.First().Length;
+            var scalers = new List<IScaler>();
+            for (var i = 0; i < columnCount; ++i)
+            {
+                var scaler = CreateScaler(i, _removedColumnsIndices);
+                var column = featuresArray.GetColumn(i);
+                var newColumn = scaler.FitTransform(column).ToArray();
+                featuresArray.SetColumn(i, newColumn);
+                scalers.Add(scaler);
+            }
+            _scalers = scalers;
+        }
+
+        /// <summary>
+        /// Нормализует индексы удалённых индексов.
+        /// </summary>
+        /// <param name="removedIndicesGroups">Группы индексов удалённых индексов.</param>
+        /// <returns>Возвращает нормализованные удалённые индексы.</returns>
+        protected IEnumerable<int> NormalizeRemovedIndices
+            (IEnumerable<IEnumerable<int>> removedIndicesGroups)
+        {
+            var result = new List<int>();
+            foreach (var removedIndices in removedIndicesGroups)
+            {
+                var newNormalizedIndices = new List<int>();
+                foreach (var index in removedIndices.Order())
+                {
+                    var normalizedIndex = index;
+                    foreach (var removedIndex in result)
+                    {
+                        if (normalizedIndex > removedIndex)
+                        {
+                            break;
+                        }
+                        ++normalizedIndex;
+                    }
+                    newNormalizedIndices.Add(normalizedIndex);
+                }
+                result.AddRange(newNormalizedIndices);
+                result = result.Order().ToList();
+            }
+            return result;
         }
 
         /// <summary>
@@ -67,12 +177,5 @@ namespace MachineLearning.Converters
                 ++index;
             }
         }
-
-        /// <summary>
-        /// Обрабатывает целевые значения элемента.
-        /// </summary>
-        /// <param name="item">Элемента.</param>
-        /// <returns>Возвращет обработанное целевое значение элемента.</returns>
-        protected abstract R ProcessTarget(D item);
     }
 }
