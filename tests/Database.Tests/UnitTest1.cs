5317b2f65169ab4363e6bb5abc64a50a72d95858
FolderMaster <99458522+FolderMaster@users.noreply.github.com>
2026-02-27 21:44:55 +0700

    Update versions and packges, add database test project.

---
+++ tests/Database.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace Database.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
